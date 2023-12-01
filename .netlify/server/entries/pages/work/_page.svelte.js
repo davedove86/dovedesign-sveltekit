ab936c24b4735355d70470c0732707ba0cf5d6f1
Dave Dove <info@dovedesign.io>
2023-12-01 11:55:54 +0000

    netlify form added

--- .netlify/server/entries/pages/work/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1 data-svelte-h="svelte-15p12fn">Our Work</h1>`;
-});
-export {
-  Page as default
-};
